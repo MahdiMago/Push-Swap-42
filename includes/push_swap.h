a283e98172a619823d5cc7ffcd581957ba0ff9f9
Mahdi Magomadov <mamagoma@c2r12p2.42nice.fr>
2025-02-12 21:01:50 +0100

    ouais

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mamagoma <mamagoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:19:34 by mamagoma          #+#    #+#             */
-/*   Updated: 2025/02/05 11:49:39 by mamagoma         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:34:00 by mamagoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ struct s_list_a
 {
 	int				value;
 	int				index;
-	struct list_a	*prev;
 	struct list_a	*next;
 };
 
@@ -32,8 +31,12 @@ struct s_list_b
 {
 	int				value;
 	int				index;
-	struct list_b	*prev;
 	struct list_b	*next;
 };
+
+char	*split_arguments(char **argv);
+int		check_caracters(char **argv);
+int		check_double_2(char *s1, char *s2);
+int		check_double(char **args);
 
 #endif
--- src/check_input.c
+++ src/check_input.c
@@ -6,22 +6,87 @@
 /*   By: mamagoma <mamagoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:10:27 by mamagoma          #+#    #+#             */
-/*   Updated: 2025/02/05 11:51:33 by mamagoma         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:33:00 by mamagoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	check_caracter(int argc, char **argv)
+char	*split_arguments(char **argv)
+{
+	char	**args;
+
+	args = ft_split(argv[1], ' ');
+	if (!args)
+		return (NULL);
+	return (args);
+}
+
+int	check_caracters(char **argv)
 {
 	int	i;
+	int	j;
 
-	i = 1;
-	while (i < argc )
+	j = 1;
+	while (argv[j])
 	{
-		if (contain_only(argv[i]) == 1)
+		i = 0;
+		while (argv[j][i])
 		{
-			
+			if (argv[j][i] >= '0' && argv[j][i] <= '9')
+				i++;
+			else if ((argv[j][i + 1] >= '0' && argv[j][i + 1] <= '9') &&
+			argv[j][i] == '-' || argv[j][i] == '+')
+				i++;
+			else
+				return (1);
 		}
+		j++;
 	}
+	return (0);
+}
+
+int	check_double_2(char *s1, char *s2)
+{
+	int	i;
+	int	n;
+
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (1);
+	i = 0;
+	n = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == s2[i])
+			n++;
+		i++;
+	}
+	if (n == ft_strlen(s1))
+		return (0);
+	return (1);
+}
+
+int	check_double(char **args)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (args[j])
+	{
+		if (check_double_2(args[j], args[i]) == 0)
+		{
+			ft_printf("il y a des doublons");
+			return (1);
+		}
+		if (args[j + 1] == NULL)
+		{
+			i++;
+			j = 0;
+		}
+		j++;
+	}
+	ft_printf("pas de doublons");
+	return (0);
 }
